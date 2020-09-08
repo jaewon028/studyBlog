aecb3b666265cc47abaf3a3cebdb0a62d6a08fff
james <jaewon028@gmail.com>
2020-09-09 02:04:16 +0900

    Route Error

--- server/app.js
+++ server/app.js
@@ -1,7 +1,39 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+// Routes
+import postRoutes from "./routes/api/post";
 
 const app = express();
+const { MONGO_URI } = config;
 
+// 서버 보안 라이브러리 적용.
+app.use(hpp());
+app.use(helmet());
+
+// cors : 브라우저가 다른 도메인이나 포트가 다른 서버에서 자원을 요청해주도록 해주는 것.
+app.use(cors({ origin: true, credentials: true }));
+// morgan : log 확인.
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Mongoose connecting Success!!"))
+  .catch((e) => console.log(e));
+
+// Use routes
 app.get("/");
+app.use("api/post", postRoutes);
 
 export default app;
