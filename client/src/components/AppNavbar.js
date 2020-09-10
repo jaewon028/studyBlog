cc941e0c36fa9307a5ff77f606cb9779bf2d9294
james <jaewon028@gmail.com>
2020-09-10 16:11:37 +0900

    Navbar done

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Navbar, NavbarToggler, Collapse, Nav } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const AppNavbar = () => {
+  return (
+    <>
+      <Navbar color="dark" dark expand="lg" className="sticky-top">
+        <Container>
+          <Link to="/" className="text-white text-decoration-none">
+            Side Project's Blog(Kanade's)
+          </Link>
+          <NavbarToggler />
+          <Collapse isOpen={true} navbar>
+            <Nav className="ml-auto d-flex justify-content-around" navbar>
+              {false ? (
+                <h1 className="text-white">authLink</h1>
+              ) : (
+                <h1 className="text-white">guestLink</h1>
+              )}
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default AppNavbar;
