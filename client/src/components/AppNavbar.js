76f6bbbd760cc6e61f93ff2862792d4a7165714b
james <jaewon028@gmail.com>
2020-09-10 22:18:13 +0900

    Register done

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,9 +1,19 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { Container, Navbar, NavbarToggler, Collapse, Nav } from "reactstrap";
+import React, { useState, useEffect, useCallback, Fragment } from "react";
+import {
+  Container,
+  Navbar,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  Form,
+  Button,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 import LoginModal from "../components/auth/LoginModal";
 import { useSelector, useDispatch } from "react-redux";
 import { LOGIN_REQUEST } from "../redux/types";
+import RegisterModal from "./auth/RegisterModal";
 const AppNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { isAuthenticated, user, userRole } = useSelector(
@@ -27,6 +37,65 @@ const AppNavbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const addPostClick = () => {};
+
+  // 주인만 인증.
+  const authLink = (
+    <Fragment>
+      <NavItem>
+        {userRole === "MainJuin" ? (
+          <Form className="col mt-2">
+            <Link
+              to="post"
+              className="btn btn-success block text-white px-3"
+              onClick={addPostClick}
+            >
+              Add post
+            </Link>
+          </Form>
+        ) : (
+          "권한 없음"
+        )}
+      </NavItem>
+      <NavItem className="d-flex justify-content-center">
+        <Form className="col mt-2">
+          {user && user.name ? (
+            <Link>
+              <Button outline color="light" className="px-3" block>
+                <strong>{user ? `Welcome ${user.name}` : ""}</strong>
+              </Button>
+            </Link>
+          ) : (
+            <Button outline color="light" className="px-3" block>
+              <strong>NOT FOUND USER</strong>
+            </Button>
+          )}
+        </Form>
+      </NavItem>
+      <NavItem>
+        <Form className="col">
+          <Link onClick={onLogout} to="#">
+            <Button outline color="light" className="mt-2" block>
+              Logout
+            </Button>
+          </Link>
+        </Form>
+      </NavItem>
+    </Fragment>
+  );
+
+  // 게스트 인증
+  const guestLink = (
+    <Fragment>
+      <NavItem>
+        <RegisterModal />
+      </NavItem>
+      <NavItem>
+        <LoginModal />
+      </NavItem>
+    </Fragment>
+  );
+
   return (
     <>
       <Navbar color="dark" dark expand="lg" className="sticky-top">
@@ -37,11 +106,7 @@ const AppNavbar = () => {
           <NavbarToggler onClick={handleToggle} />
           <Collapse isOpen={isOpen} navbar>
             <Nav className="ml-auto d-flex justify-content-around" navbar>
-              {isAuthenticated ? (
-                <h1 className="text-white">authLink</h1>
-              ) : (
-                <LoginModal />
-              )}
+              {isAuthenticated ? authLink : guestLink}
             </Nav>
           </Collapse>
         </Container>
