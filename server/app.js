b00a1e9b73719c256b8fa291c3538590c140bf17
james <jaewon028@gmail.com>
2020-09-18 03:02:12 +0900

    CSP error try v22

--- server/app.js
+++ server/app.js
@@ -20,31 +20,33 @@ const prod = process.env.NODE_ENV === "production";
 
 // 서버 보안 라이브러리 적용.
 app.use(hpp());
-app.use(helmet());
-// app.use(helmet({
-//   contentSecurityPolicy: false,
-// }));
+// app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 // cors : 브라우저가 다른 도메인이나 포트가 다른 서버에서 자원을 요청해주도록 해주는 것.
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 
-// if (prod) {
-//   app.use(
-//     cors({
-//       origin: ["https://kanadestudyblog.net", /\.kanadestudyblog.net$/],
+if (prod) {
+  app.use(
+    cors({
+      origin: ["https://kanadestudyblog.net", /\.kanadestudyblog.net$/],
 
-//       credentials: true,
-//     })
-//   );
-// } else {
-//   app.use(morgan("dev"));
-//   app.use(
-//     cors({
-//       origin: true,
-//       credentials: true,
-//     })
-//   );
-// }
+      credentials: true,
+    })
+  );
+} else {
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+}
 
 // morgan : log 확인.
 
@@ -63,15 +65,15 @@ mongoose
 // Use routes
 
 // Use routes
-// app.all("*", (req, res, next) => {
-//   let protocol = req.headers["x-forward-proto"] || req.protocol;
-//   if (protocol === "https") {
-//     next();
-//   } else {
-//     let to = `https://${req.hostname}${req.url}`;
-//     res.redirect(to);
-//   }
-// });
+app.all("*", (req, res, next) => {
+  let protocol = req.headers["x-forward-proto"] || req.protocol;
+  if (protocol === "https") {
+    next();
+  } else {
+    let to = `https://${req.hostname}${req.url}`;
+    res.redirect(to);
+  }
+});
 
 // app.get("/");
 app.use("/api/post", postRoutes);
@@ -79,11 +81,11 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/search", searchRoutes);
 
-// if (prod) {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-//   });
-// }
+if (prod) {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
 
 export default app;
